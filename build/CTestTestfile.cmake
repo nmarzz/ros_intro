6ce4d7d3926cc7cd9b7b1809c51f71a70b9592d9
noah <ndmarshall97@gmail.com>
2022-01-28 18:58:41 -0500

    setup env

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/noah/ros_intro/src
+# Build directory: /home/noah/ros_intro/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/noah/ros_intro/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/noah/ros_intro/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/noah/ros_intro/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/noah/ros_intro/src:$ROS_PACKAGE_PATH"
